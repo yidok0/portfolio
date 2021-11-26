3e86ee2e7bb762b87318474be74a287d57aa633c
yidok <idk1704@naver.com>
2021-11-26 16:18:07 +0900

    로딩화면추가

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,10 @@
 $(function(){
+    //로딩화면
+    setTimeout(function() {
+        $(".loading").fadeOut();
+    }, 4000);
+
+
     // isotope 플러그인 시작 세팅
     $('.tabList').isotope({
         // options
@@ -52,6 +58,28 @@ $(function(){
 	    navigationPosition: 'right',
     });
 
+});
+// AOS 추가
+AOS.init();
+
+// SWIPER 스와이퍼
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed:1000,
+    autoplay: {
+        delay: 4000,
+    },    
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    },        
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
 });
 
 // 스크롤하면 헤더 디자인 변경
